d20e023be4f07b33d7876b718074af1954ddd3a8
Bismillah <cupertino@Matraims-MacBook-Pro.local>
2023-11-14 20:39:13 +0600

    test

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import GppMaybeIcon from '@mui/icons-material/GppMaybe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ToastContainer position="bottom-left" limit={2} icon={<GppMaybeIcon />}  />
     <App />
   </React.StrictMode>
 );
